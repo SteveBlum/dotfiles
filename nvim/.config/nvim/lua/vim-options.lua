6cd9c1c23bb6506d9f96d92f98053500d94fdf50
Steve Blum <steve.blum@live.de>
2025-08-16 15:30:52 +0200

    Implemented vim key navigation in nvim, tmux and hyprland

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -5,6 +5,12 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
+-- Navigate vim panes compatible with tmux
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
 -- Copy to clipboard
 vim.keymap.set('v', '<leader>y', '"+y', { noremap = true })
 vim.keymap.set('n', '<leader>Y', '"+yg_', { noremap = true })
