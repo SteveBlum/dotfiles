426ed117f881dd7d37d0998cc9ff4d96d243c6b6
Steve Blum <steve.blum@live.de>
2025-09-13 01:31:00 +0200

    Added firefox with Wavefox

---
+++ firefox/.mozilla/firefox/profile.default/user.js
@@ -0,0 +1,8 @@
+user_pref("WaveFox.Linux.Transparency.Enabled", true);
+user_pref("WaveFox.Toolbar.Transparency", 1);
+user_pref("WaveFox.WebPage.Background.Saturation", 1);
+user_pref("WaveFox.WebPage.Transparency", 1);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.tabs.inTitlebar", 1);
+user_pref("browser.tabs.allow_transparent_browser", true);
+
