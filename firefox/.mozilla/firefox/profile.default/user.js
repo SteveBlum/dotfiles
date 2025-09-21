2afc331bb263bb6212ae354e8463420bdc224d0b
Steve Blum <steve.blum@live.de>
2025-09-21 14:37:15 +0200

    Fixed firefox plugins not being visible

--- firefox/.mozilla/firefox/profile.default/user.js
+++ firefox/.mozilla/firefox/profile.default/user.js
@@ -1,7 +1,7 @@
 user_pref("WaveFox.Linux.Transparency.Enabled", true);
 user_pref("WaveFox.Toolbar.Transparency", 1);
 user_pref("WaveFox.WebPage.Background.Saturation", 1);
-user_pref("WaveFox.WebPage.Transparency", 1);
+user_pref("WaveFox.WebPage.Transparency", 0);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.tabs.inTitlebar", 1);
 user_pref("browser.tabs.allow_transparent_browser", true);
