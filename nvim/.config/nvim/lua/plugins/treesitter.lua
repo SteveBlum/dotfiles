a3036890b19c73ca290f12cdf1e2278948fe7bba
Steve Blum <steve.blum@live.de>
2025-12-29 18:26:02 +0100

    chore: migration from nvim-treesitter master branch to main

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,29 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "javascript", "markdown", "rust", "yaml", "hurl" },
-			highlight = { enable = true },
-			indent = { enable = true },
+		local ts = require("nvim-treesitter")
+		local parsers = {
+			"lua",
+			"javascript",
+			"markdown",
+			"rust",
+			"yaml",
+			"hurl",
+		}
+
+		for _, parser in ipairs(parsers) do
+			ts.install(parser)
+		end
+
+		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo[0][0].foldmethod = "expr"
+		vim.api.nvim_command("set nofoldenable")
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = parsers,
+			callback = function()
+				vim.treesitter.start()
+			end,
 		})
 	end,
 }
