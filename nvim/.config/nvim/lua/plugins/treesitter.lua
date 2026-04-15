73d398f5a6b9f288ec4fefd839fc121f2cbe8d4d
Steve Blum <steve.blum@live.de>
2026-04-16 00:45:55 +0200

    fix: helm chart template files were not parsed correctly

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -11,6 +11,7 @@ return {
 			"rust",
 			"yaml",
 			"hurl",
+      "helm"
 		}
 
 		for _, parser in ipairs(parsers) do
@@ -20,6 +21,23 @@ return {
 		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 		vim.wo[0][0].foldmethod = "expr"
 		vim.api.nvim_command("set nofoldenable")
+
+    -- helm chart template handling
+    vim.filetype.add({
+      extension = {
+        yaml = function(path, bufnr)
+          if vim.fn.search("{{", "nw") ~= 0 then
+            return "helm"
+          end
+          return "yaml"
+        end,
+      },
+      pattern = {
+        [".*/templates/.*%.yaml"] = "helm",
+        [".*/templates/.*%.tpl"] = "helm",
+        ["helmfile.*%.yaml"] = "helm",
+      },
+    })
 
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = parsers,
