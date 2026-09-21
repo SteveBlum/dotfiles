6559828dcc9406e6708de06bf2fe2875cc1f9b8a
Steve Blum <steve.blum@live.de>
2026-09-21 09:41:42 +0200

    feat: added gitlab plugin for neovim

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -11,5 +11,17 @@ return {
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 			vim.keymap.set("n", "<leader>gb", ":Gitsigns blame<CR>", {})
 		end,
-	}
+	},
+  {
+    "harrisoncramer/gitlab.nvim",
+    branch = "main",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "dlyongemallo/diffview-plus.nvim", -- Maintained fork of "sindrets/diffview.nvim".
+      "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
+      "nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
+    },
+    ---@type GitlabSettings
+    opts = {}, -- Your configuration
+  }
 }
